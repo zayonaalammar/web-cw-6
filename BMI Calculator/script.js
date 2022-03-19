b1974442af16f8868279cb453be1938fb8fdaf22
zayona <alzain05@icloud.com>
2022-03-19 19:07:54 +0300

    done

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,17 @@
-// هنا سوف تنشئ دالة calculate 
+// هنا سوف تنشئ دالة calculate
+function calculate() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+
+  let BMI = height / (weight * weight);
+
+  let status = "";
+  if (BMI < 18.5) {
+    status = "تعاني من نقص في الوزن";
+  } else if (BMI < 25) {
+    status = "وزنك صحي";
+  }
+  BMI = BMI.toFixed(2);
+  document.getElementById("result").innerHtml = BMI;
+  document.getElementsByClassName("comment")[0].innerHTML = status;
+}
